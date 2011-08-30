2e1640f750065757a27711b863667b9c964a0465
Louis Stowasser <louisstow@gmail.com>
2011-08-31 09:25:21 +1000

    * Isometric

--- src/isometric.js
+++ src/isometric.js
@@ -1,46 +1,56 @@
-Crafty.extend({
+/**@
+* #RectIsometric
+* @category 2D
+* Place entities in a 45deg rectangle isometric grid.
+*/
+Crafty.c("RectIsometric", {
+	_tile: 0,
+	_z: 0,
+	
 	/**@
-	* #Crafty.isometric
-	* @category 2D
+	* #.RectIsometric
+	* @comp RectIsometric
+	* @sign public this .RectIsometric(Number tile)
+	* @param tile -  Size of the isometric tiles
 	* Place entities in a 45deg isometric fashion.
 	*/
-	isometric: {
-		_tile: 0,
-		_z: 0,
+	RectIsometric: function(tile, z) {
+		this._tile = tile;
 		
-		/**@
-		* #Crafty.isometric.size
-		* @comp Crafty.isometric
-		* @sign public this Crafty.isometric.size(Number tileSize)
-		* @param tileSize - The size of the tiles to place.
-		* Method used to initialize the size of the isometric placement.
-		* Recommended to use a size alues in the power of `2` (128, 64 or 32). 
-		* This makes it easy to calculate positions and implement zooming.
-		* @see Crafty.isometric.place
-		*/
-		size: function(tile) {
-			this._tile = tile;
-			return this;
-		},
+		return this;
+	},
 		
-		/**@
-		* #Crafty.isometric.place
-		* @comp Crafty.isometric
-		* @sign public this Crafty.isometric.size(Number x, Number y, Number z, Entity tile)
-		* @param x - The `x` position to place the tile
-		* @param y - The `y` position to place the tile
-		* @param z - The `z` position or height to place the tile
-		* @param tile - The entity that should be position in the isometric fashion
-		* Use this method to place an entity in an isometric grid.
-		* @see Crafty.isometric.size
-		*/
-		place: function(x,y,z, obj) {
-			var m = x * this._tile + (y & 1) * (this._tile / 2),
-				n = y * this._tile / 4,
-				n = n - z * (this._tile / 2);
-				
-			obj.attr({x: m  + Crafty.viewport.x, y: n  + Crafty.viewport.y}).z += z;
-			return this;
-		}
+	/**@
+	* #.size
+	* @comp RectIsometric
+	* @sign public this .size(Number tileSize)
+	* @param tileSize - The size of the tiles to place.
+	* Method used to initialize the size of the isometric placement.
+	* Recommended to use a size alues in the power of `2` (128, 64 or 32). 
+	* This makes it easy to calculate positions and implement zooming.
+	* @see .place
+	*/
+	size: function(tile) {
+		this._tile = tile;
+		return this;
+	},
+		
+	/**@
+	* #.place
+	* @comp RectIsometric
+	* @sign public this .place(Number x, Number y, Number z)
+	* @param x - The `x` position to place the tile
+	* @param y - The `y` position to place the tile
+	* @param z - The `z` position or height to place the tile
+	* Use this method to place itself in an isometric grid.
+	* @see .size
+	*/
+	place: function(x, y, z) {
+		var m = x * this._tile + (y & 1) * (this._tile / 2),
+			n = y * this._tile / 4,
+			n = n - z * (this._tile / 2);
+			
+		this.attr({x: m  + Crafty.viewport.x, y: n  + Crafty.viewport.y}).z += z;
+		return this;
 	}
 });
