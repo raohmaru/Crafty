05b4cf24caa869781f2be5317eb730cde3a7fd97
sorenbs <sorenbs@gmail.com>
2011-09-19 13:35:38 -0500

    this wen missing i guess

--- src/loader.js
+++ src/loader.js
@@ -54,7 +54,19 @@ Crafty.extend({
 		for(;i<l;++i) {
 			current = data[i];
 			ext = current.substr(current.lastIndexOf('.')+1).toLowerCase();
-			
+
+			if (Crafty.support.audio && (ext === "mp3" || ext === "wav" || ext === "ogg" || ext === "mp4")) {
+			    obj = new Audio(current);
+			    //Chrome doesn't trigger onload on audio, see http://code.google.com/p/chromium/issues/detail?id=77794
+			    if (navigator.userAgent.indexOf('Chrome') != -1) j++;
+			} else if (ext === "jpg" || ext === "jpeg" || ext === "gif" || ext === "png") {
+			    obj = new Image();
+			    obj.src = current;
+			} else {
+			    total--;
+			    continue; //skip if not applicable
+			}
+
 			//add to global asset collection
 			this.assets[current] = obj;
 			
