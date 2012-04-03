470ba01ba6c7e6e1314bd86ae34cc5b27263c963
MPetrovic <mpetrovic@iq.harvard.edu>
2012-04-03 13:37:53 -0400

    Begin function declarations in camera.

--- src/camera.js
+++ src/camera.js
@@ -12,12 +12,12 @@
  * Camera types:
  * 	Cameras have several types they can be, which affects how they render the game world.
  *	Types:
- *		Top - Renders the world in a top-down, birds-eye view.
- *		Side - Renders the world from the side, like Mario.
- *		Isometric - Renders the world from a 45 degree angle. Tiles exist as single sprites. (FF Tactics)
- *		IsometricFaces - Renders the world from a 45 degree angle. Tiles exist as boxes with different sprites for the sides (Minecraft)
- *		3DSquare - Renders the world in full 3D, using DOM or SVG elements to create the world itself. All objects exist as faces. (ex. A cube would be 6 DOM elements.) 
- *		3DCanvas - Renders the world using WebGL. Support for this is lacking in browsers, so it isn't recommended.
+ *		Top - Renders the world in a top-down, birds-eye view. Orthographic
+ *		Side - Renders the world from the side, like Mario. Orthographic
+ *		Isometric - Renders the world from a 45 degree angle. Tiles exist as single sprites. (FF Tactics) Orthographic
+ *		IsometricFaces - Renders the world from a 45 degree angle. Tiles exist as boxes with different sprites for the sides (Minecraft) Orthographic
+ *		3DSquare - Renders the world in full 3D, using DOM or SVG elements to create the world itself. All objects exist as faces. (ex. A cube would be 6 DOM elements.) Perspective
+ *		3DCanvas - Renders the world using WebGL. Support for this is lacking in browsers, so it isn't recommended. Perspective.
  *
  * Camera Options:
  *	Cameras accept an optional object containing options to change from defaults.
@@ -27,3 +27,95 @@
  *
  * 
  */
+(function (Crafty) {
+	Crafty.extend({
+		camera: function (label, type, options) {
+			return Crafty.camera[label] = new Crafty.camera.fn.init(type, options);
+		}
+	});
+	
+	Crafty.camera.fn = {
+		active: false,
+		x: 0,
+		y: 0,
+		z: 0,
+		type: "",
+		changed: true,
+		canvas: false,
+		/**
+		 * Constructor. Should never be invoked directly.
+		 */
+		init: function(type, options) {
+			this.type = type;
+			this.data = {};
+			this.target = {
+				x: 0,
+				y: 0,
+				z: 0
+			};
+		},
+		
+		/**
+		 * Moves the camera 
+		 */
+		move: function (x, y, z) {
+			this.moveTo(this.x + x, this.y + y, this.z + z);
+			return this;
+		},
+		
+		moveTo: function (x, y, z) {
+			if (isFinite(x)) this.x = parseInt(x);
+			if (isFinite(y)) this.y = parseInt(y);
+			if (isFinite(z)) this.z = parseInt(z);
+			return this;
+		},
+		
+		render: function () {
+			if (!this.active) return;
+			// pre render logic
+			var data;
+			
+			switch (this.type) {
+				case "Top": 
+					topdown(data);
+					break;
+				case "Side":
+					sideview(data);
+					break;
+				case "Isometric":
+					isometric(data);
+					break;
+				case "IsometricFaces":
+					isofaces(data);
+					break;
+				case "3DSquare":
+					dom3D(data);
+					break;
+				case "3DFull":
+					full3D(data);
+					break;
+			}
+			return this;
+		}
+	};
+	Crafty.camera.fn.init.prototype = Crafty.camera.fn;
+			
+	// render implementations go here
+	function topdown(data) {
+	}
+	
+	function sideview(data) {
+	}
+	
+	function isometric(data) {
+	}
+	
+	function isofaces(data) {
+	}
+	
+	function dom3D(data) {
+	}
+	
+	function full3D(data) {
+	}
+})(Crafty);
