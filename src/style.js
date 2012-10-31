15a30725d64d1084dfaec09579d216762c568414
Matt Petrovic <mpetrovic@iq.harvard.edu>
2012-10-31 12:30:33 -0400

    CSS handling is ready for testing.

--- src/style.js
+++ src/style.js
@@ -1,3 +1,8 @@
+	/**@
+	 * #Crafty.style
+	 * @category CSS
+	 * Helper methods for adding css to a 'stylesheet'. 
+	 */
 	Crafty.extend({
 		style: (function () {
 			var styles = {},
@@ -13,28 +18,75 @@
 			sheet = document.styleSheets[document.styleSheets.length - 1];
 			
 			function writeStyles() {
-				var i, j, str;
+				var i, j, str, del;
 				
 				for (i in styles) {
+					del = true
 					// W3C method
 					if ('insertRule' in s) {
 						str = i + ' { ';
 						for (j in styles[i]) {
-							str += j+': '+i+'; ';
+							if (styles[i][j] && j != 'index') {
+								str += j+': '+styles[i][j]+'; ';
+								del = false;
+							}
 						}
 						str += '}';
 						if (typeof styles[i].index != 'undefined') {
-							s.insertRule(str, styles[i].index);
+							if (del) {
+								s.deleteRule(styles[i].index);
+							}
+							else {
+								s.insertRule(str, styles[i].index);
+							}
+						}
+						else {
+							if (!del) {
+								styles[i].index = s.insertRule(str, s.cssRules.length);
+							}
+							// the style hasn't been added yet, so there's nothing to delete
 						}
 					}
 					// IE bullcrap
 					else {
 						str = '';
+						for (j in styles[i]) {
+							if (styles[i][j] && j != 'index') {
+								str += j+': '+styles[i][j]+'; ';
+								del = false;
+							}
+						}
+						if (typeof styles[i].index == 'undefined') {
+							styles[i].index = s.rules.length;
+							s.addRule(i, str, styles[i].index);
+						}
+						else if (del) {
+							s.removeRule(styles[i].index);
+						}
+						else {
+							s.addRule(i, str, styles[i].index);
+						}
 					}
 				}
 			}
 			
 			return {
+				/**@
+				 * #Crafty.style.add
+				 * @comp Crafty.style
+				 * @sign Crafty.style.add(String rule, String property, String value)
+				 * @sign Crafty.style.add(String rule, Object property)
+				 * @param String rule - the css selector. can be any valid css selector
+				 * @param String property - The css property to add a rule for
+				 * @param String value - The value of the property. Must include unit.
+				 * @param Object property - An object of property:value pairs. Shorthand for several add calls
+				 *
+				 * Adds a css selector, and sets one or more properties and their values
+				 * @example 
+				 *   Crafty.style.add('.Spatial', 'position', 'absolute');
+				 * @example
+				 *   Crafty.style.add('.camera.dthree .layer.dthree', {position: 'absolute', perspective: '1000px'});
+				 */
 				add: function(rule, property, value) {
 					var i = '', changes = false;
 					
